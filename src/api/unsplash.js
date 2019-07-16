405c834238a1fc0fc80ec71058e4dc4a7cb3340f
lewismuse <cai.gyuan@gmail.com>
2019-07-15 21:24:03 -0500

    done

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL:'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID 59f9d0817059241bd1ef22ecbc0d7027b621b035bbac1a6887baaacbb334337f'
+        //put unsplash api auth after Client-ID
+        Authorization: 'Client-ID '
     }
 });
